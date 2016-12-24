75c83420e8760fb0415c286fa4b1dd7eb0a847ce
Tasha25 <greennatasha@aol.com>
2016-12-24 10:49:01 -0500

    Add voting design and capabilities

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,13 @@
 class VotesController < ApplicationController
+  #to store votes that have been submitted
   def create
-    #to store votes that have been submitted
+    #we find the appropriate story record using the unique ID of Story for which a vote has been cast. This ID is given to us by Rails in the form of params[:story_id], since params[:id] is in this case reserved for a potential ID of a Vote object
     @story = Story.find(params[:story_id])
+    #creates and saves a new Vote
     @story.votes.create
+    respond_to do |format|
+      format.html { redirect_to @story, notice: 'Vote was successfully created.' }
+      format.js {}
+    end
   end
 end
